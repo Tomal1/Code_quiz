b18dec6646f06b31cf610abdbe80a1c471b2e363
Minhazul Islam <minhazulislam@hotmail.com>
2022-10-18 18:12:02 +0100

    HTML file and JS with array of questions

---
+++ quiz.js
@@ -0,0 +1,16 @@
+let q1 = "how old is Tomal?";
+let q2 = "where did Tomal live before moving to Birmingham?";
+let q3 = "where about in birmingham dose Tomal live?";
+let q4 = "what job did Tomal do before joining the coding Bootcamp?";
+
+
+let QuestArr = [q1, q2, q3, q4];
+
+let question = document.querySelector("#question");
+
+question.innerHTML = q1;
+
+let answer = document.querySelector("#answer");
+
+answer.innerHTML = "this is where it will tell you if you are right or wrong";
+
