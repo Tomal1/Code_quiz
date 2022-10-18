a107290de95bd2f0e5f1889ea4c76741b85dbeb6
Minhazul Islam <minhazulislam@hotmail.com>
2022-10-18 20:16:04 +0100

    displaying first question with 4 answers to select

--- quiz.js
+++ quiz.js
@@ -5,16 +5,10 @@ let q4 = "what job did Tomal do before joining the coding Bootcamp?";
 
 let questArr = [q1, q2, q3, q4];
 
-let a1 = 34;
-let a2 = "London";
-let a3 = "Halesowen";
-let a4 = "recovery driver";
-let a5 = "True";
-let a6 = "False";
-
-let ansArr = [a1, a2, a3, a4, a5, a6];
-
-
+let a1 = [32, 33, 34, 35];
+let a2 = ["Leicester", "London", "Newport", "Swansea"];
+let a3 = ["Small Heath", "Halesowen", "Smithwick", "Digbeth"];
+let a4 = ["recovery driver", "police", "lorry driver", "chef"];
 
 
 let question = document.querySelector("#question");
@@ -24,4 +18,21 @@ question.innerHTML = q1;
 let answer = document.querySelector("#answer");
 
 answer.innerHTML = "this is where it will tell you if you are right or wrong";
+
+
+let btn1 = document.querySelector("#span1");
+let btn2 = document.querySelector("#span2");
+let btn3 = document.querySelector("#span3");
+let btn4 = document.querySelector("#span4");
+
+let btnAry = [btn1, btn2, btn3, btn4];
+
+
+
+for(let i = 0; btnAry.length; i++){
+     btnAry[i].textContent = a1[i]; 
+}
+
+
+
 
