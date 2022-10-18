367c8303ecab98b84bed12c92ae486eafb297673
Minhazul Islam <minhazulislam@hotmail.com>
2022-10-18 18:18:45 +0100

    answers array added

--- quiz.js
+++ quiz.js
@@ -3,8 +3,19 @@ let q2 = "where did Tomal live before moving to Birmingham?";
 let q3 = "where about in birmingham dose Tomal live?";
 let q4 = "what job did Tomal do before joining the coding Bootcamp?";
 
+let questArr = [q1, q2, q3, q4];
 
-let QuestArr = [q1, q2, q3, q4];
+let a1 = 34;
+let a2 = "London";
+let a3 = "Halesowen";
+let a4 = "recovery driver";
+let a5 = "True";
+let a6 = "False";
+
+let ansArr = [a1, a2, a3, a4, a5, a6];
+
+
+
 
 let question = document.querySelector("#question");
 
