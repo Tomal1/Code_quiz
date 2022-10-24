1097704c313405882791c5b29171e4a40d3b0cf2
Minhazul Islam <minhazulislam@hotmail.com>
2022-10-24 21:21:22 +0100

    object added, array removed and readme added but still uncompleated

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# challenge_WK4_code_quiz
-code quiz
+# Code Quiz
+
+## Description
+This is the Forth assignment, the task was to create a functional app from scratch which provides a series of questions and must be answered correctly to win/complete the app/game.
+
+
+deployed website URL: 
+
+gitHub repo URL: 
+
+## Usage
+
+When the user clicks on the start button, a series of questions and four different answers for each questions are displayed. 
+If the correct answer is selected then the message "correct" will be displayed otherwise "wrong" is displayed.
+If all 4 questions are answered correctly, the message; "you win" will be displayed.
+
+the Quiz is not yet completed and will be submitted again!
+
+//![alt text](assets/images/picture.png)
+
+
+## License
+
+Please refer to the LICENSE in the repo.
--- quiz.js
+++ quiz.js
@@ -1,16 +1,33 @@
-let q1 = "how old is Tomal?";
-let q2 = "where did Tomal live before moving to Birmingham?";
-let q3 = "where about in birmingham dose Tomal live?";
-let q4 = "what job did Tomal do before joining the coding Bootcamp?";
-
-let questArr = [q1, q2, q3, q4];
 
 let questionSpan = document.querySelector("#question");
 
-let a1 = [32, 33, 34, 35];
-let a2 = ["Leicester", "London", "Newport", "Swansea"];
-let a3 = ["Small Heath", "Halesowen", "Smithwick", "Digbeth"];
-let a4 = ["recovery driver", "police", "lorry driver", "chef"];
+const tomal = [
+  {
+    Q: "to start the quiz press any button",
+    A: ["start button", "start button", "start button", "start button"],
+    CA: "start button",
+  },
+  {
+    Q: "how old is Tomal?",
+    A: ["32", "33", "34", "35"],
+    CA: "34",
+  },
+  {
+    Q: "where did Tomal live before moving to Birmingham?",
+    A: ["Leicester", "London", "Newport", "Swansea"],
+    CA: "London",
+  },
+  {
+    Q: "where about in birmingham dose Tomal live?",
+    A: ["Small Heath", "Halesowen", "Smithwick", "Digbeth"],
+    CA: "Halesowen",
+  },
+  {
+    Q: "what job did Tomal do before joining the coding Bootcamp?",
+    A: ["recovery driver", "police", "lorry driver", "chef"],
+    CA: "recovery driver",
+  }
+];
 
 let answerSpan = document.querySelector("#answer");
 
@@ -20,88 +37,47 @@ let btn3 = document.querySelector(".btn3");
 let btn4 = document.querySelector(".btn4");
 
 let btnAry = [btn1, btn2, btn3, btn4];
-/*function which takes  arguments to populate the button and display the 
-correct question*/
-function nextQues(arg){
 
-    if(arg === q1){
-        questionSpan.innerHTML = q1;
-        for(let i = 0; i < btnAry.length; i++){
-            btnAry[i].textContent = a1[i];
-        }
+let counter = 0;
 
-        for(let i=0; i<btnAry.length; i++){
-            function someFunc(arg){
-                if(arg === btnAry[2]){
-                    answerSpan.innerHTML = "correct";
-                }else{
-                    answerSpan.innerHTML = "wrong";
-                }
-            }
-        
-            btnAry[i].addEventListener("click", function(){
-                someFunc(btnAry[i]);       
-            })
-        }
-    } else if (arg === q2){
-        questionSpan.innerHTML = q2;
-        for(let i = 0; i < btnAry.length; i++){
-            btnAry[i].textContent = a2[i];
-        }
 
-        for(let i=0; i<btnAry.length; i++){
-            function someFunc(arg){
-                if(arg === btnAry[1]){
-                    answerSpan.innerHTML = "correct";
-                }else{
-                    answerSpan.innerHTML = "wrong";
-                }
-            }
-        
-            btnAry[i].addEventListener("click", function(){
-                someFunc(btnAry[i]);       
-            })
-        }
-    } else if (arg === q3){
-        questionSpan.innerHTML = q3;
-        for(let i = 0; i < btnAry.length; i++){
-            btnAry[i].textContent = a3[i];
-        }
 
-        for(let i=0; i<btnAry.length; i++){
-            function someFunc(arg){
-                if(arg === btnAry[1]){
-                    answerSpan.innerHTML = "correct";
-                }else{
-                    answerSpan.innerHTML = "wrong";
-                }
-            }
-        
-            btnAry[i].addEventListener("click", function(){
-                someFunc(btnAry[i]);       
-            })
-        }
-    } else if (arg === q4){
-        questionSpan.innerHTML = q4;
-        for(let i = 0; i < btnAry.length; i++){
-            btnAry[i].textContent = a4[i];
-        }
 
-        for(let i=0; i<btnAry.length; i++){
-            function someFunc(arg){
-                if(arg === btnAry[0]){
-                    answerSpan.innerHTML = "correct";
-                }else{
-                    answerSpan.innerHTML = "wrong";
-                }
-            }
-        
-            btnAry[i].addEventListener("click", function(){
-                someFunc(btnAry[i]);       
-            })
-        }
-    }
+
+function nextQues(object) {
+  questionSpan.innerHTML = object.Q;
+  for (let i = 0; i < btnAry.length; i++) {
+    btnAry[i].textContent = object.A[i];
+  }
 }
 
-nextQues(q1);
+ function someFunc(arg, object) {
+    if (arg === object.CA) {
+    answerSpan.innerHTML = "correct";
+      counter++;
+
+      if(counter >= tomal.length){
+        questionSpan.innerHTML = "";
+        return answerSpan.innerHTML ="you win";   
+      }
+      nextQues(tomal[counter]);
+
+    } else {
+      answerSpan.innerHTML = "wrong";
+    }
+    console.log(counter);
+  }
+
+  document.querySelector("#score");
+
+  console.log(counter);
+
+  for (let i = 0; i < btnAry.length; i++) {
+    btnAry[i].addEventListener("click", function () {
+      someFunc(btnAry[i].textContent, tomal[counter]);
+    });
+  }
+
+nextQues(tomal[0]);
+
 
