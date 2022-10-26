e8d9013e609e36e4e87f3e11c5244e18fe41c187
Minhazul Islam <minhazulislam@hotmail.com>
2022-10-26 02:38:55 +0100

    compleated work without clear timer function and css

--- quiz.js
+++ quiz.js
@@ -1,30 +1,33 @@
-
-const ticker = setInterval(counting, 1000);
-
-let timer = document.querySelector("#time");
-let countdown = 15;
- 
-function counting(){
-
-  countdown = countdown - 1;
-  if(countdown > 0){
-    return timer.innerHTML = countdown
-  } else if(countdown == 0){
-    return timer.innerHTML = "you lose";
-  }
-
-}
-
-
-
+let answerSpan = document.querySelector("#answer");
 let questionSpan = document.querySelector("#question");
+let btnCon = document.querySelector("#btn_con");
+
+let btn1 = document.querySelector(".btn1");
+let btn2 = document.querySelector(".btn2");
+let btn3 = document.querySelector(".btn3");
+let btn4 = document.querySelector(".btn4");
+
+let btnAry = [btn1, btn2, btn3, btn4];
+
+function starting() {
+  questionSpan.innerHTML = "press start button to play Code Quiz";
+  let rule = (answerSpan.innerHTML =
+    "you will be penalised 3 seconds for wrong selections");
+  btnCon.style.visibility = "hidden";
+
+  let newBtn = document.createElement("button");
+  newBtn.innerHTML = "start button";
+  document.body.appendChild(newBtn);
+
+  newBtn.addEventListener("click", function () {
+    const ticker = setInterval(counting, 1000);
+    newBtn.remove();
+    nextQues(tomal[0]);
+  });
+}
+starting();
 
 const tomal = [
-  {
-    Q: "to start the quiz press start button",
-    A: ["start button", "start button", "start button", "start button"],
-    CA: "start button",
-  },
   {
     Q: "how old is Tomal?",
     A: ["32", "33", "34", "35"],
@@ -44,55 +47,53 @@ const tomal = [
     Q: "what job did Tomal do before joining the coding Bootcamp?",
     A: ["recovery driver", "police", "lorry driver", "chef"],
     CA: "recovery driver",
-  }
+  },
 ];
-
-let answerSpan = document.querySelector("#answer");
-
-let btn1 = document.querySelector(".btn1");
-let btn2 = document.querySelector(".btn2");
-let btn3 = document.querySelector(".btn3");
-let btn4 = document.querySelector(".btn4");
-
-let btnAry = [btn1, btn2, btn3, btn4];
 
 let counter = 0;
 
 function nextQues(object) {
+  btnCon.style.visibility = "visible";
   questionSpan.innerHTML = object.Q;
   for (let i = 0; i < btnAry.length; i++) {
     btnAry[i].textContent = object.A[i];
   }
 }
 
- function someFunc(arg, object) {
-    if (arg === object.CA) {
+let timer = document.querySelector("#time");
+let countdown = 16;
+
+function counting() {
+  countdown = countdown - 1;
+  if (countdown > 0) {
+    timer.innerHTML = countdown;
+  } else if (countdown == 0) {
+    timer.innerHTML = "you lose";
+    btnCon.style.visibility = "hidden";
+    questionSpan.style.visibility = "hidden";
+    answerSpan.style.visibility = "hidden";
+  }
+}
+
+function someFunc(arg, object) {
+  if (arg === object.CA) {
     answerSpan.innerHTML = "correct";
-      counter++;
+    counter++;
 
-      if(counter >= tomal.length){
-        questionSpan.innerHTML = "";
-        return answerSpan.innerHTML ="you win";   
-      }
-      nextQues(tomal[counter]);
-
-    } else {
-      answerSpan.innerHTML = "wrong";
-      countdown = countdown - 4;
+    if (counter >= tomal.length) {
+      btnCon.style.visibility = "hidden";
+      questionSpan.style.visibility = "hidden";
+      return (answerSpan.innerHTML = "you win");
     }
-    console.log(counter);
+    nextQues(tomal[counter]);
+  } else {
+    answerSpan.innerHTML = "wrong";
+    countdown = countdown - 3;
   }
+}
 
-  document.querySelector("#score");
-
-  console.log(counter);
-
-  for (let i = 0; i < btnAry.length; i++) {
-    btnAry[i].addEventListener("click", function () {
-      someFunc(btnAry[i].textContent, tomal[counter]);
-    });
-  }
-
-nextQues(tomal[0]);
-
-
+for (let i = 0; i < btnAry.length; i++) {
+  btnAry[i].addEventListener("click", function () {
+    someFunc(btnAry[i].textContent, tomal[counter]);
+  });
+}
